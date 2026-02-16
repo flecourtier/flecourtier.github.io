24024e8493ee54f339fa8ebd207c5840c04c4d9f
flecourtier <lecourtier.frederique@gmail.com>
2026-02-16 16:32:30 +0100

    Update map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,44 +1,50 @@
 var addressPoints = [
   {
-    "description": "Enriching continuous Lagrange finite element approximation spaces using neural networks<br />IMAG, Montpellier, France",
-    "latitude": 43.3121162,
-    "longitude": 3.5457254,
-    "type": "talk",
-    "color": "#D62828"
-  },
-  {
     "description": "Combining Finite Element Methods and Neural Networks to Solve Elliptic Problems on 2D Geometries<br />ENSAM, Paris, France",
     "latitude": 48.8334984,
     "longitude": 2.3583612,
     "type": "talk",
-    "color": "#D62828"
+    "color": "#D62828",
+    "permalink": "/talks/2025-02-20-DTE_AICOMAS"
   },
   {
     "description": "Enriching continuous Lagrange finite element approximation spaces using neural networks<br />McGill University, Montreal, Canada",
     "latitude": 45.5068861,
     "longitude": -73.5787118,
     "type": "talk",
-    "color": "#D62828"
+    "color": "#D62828",
+    "permalink": "/talks/2025-07-15-ICOSAHOM"
   },
   {
-    "description": "Enriching continuous Lagrange finite element approximation spaces using neural networks<br />Politecnico di Milano, Milan, Italy",
-    "latitude": 45.4800101,
-    "longitude": 9.2282387,
-    "type": "poster",
-    "color": "#2E5CA6"
-  },
-  {
-    "description": "Combining Finite Element methods and Neural Networks to solve elliptic problem on complex 2D geometries<br />UFR, Strasbourg, France",
-    "latitude": 48.5598541,
-    "longitude": 7.7445386,
-    "type": "poster",
-    "color": "#2E5CA6"
+    "description": "Enriching continuous Lagrange finite element approximation spaces using neural networks<br />IMAG, Montpellier, France",
+    "latitude": 43.3121162,
+    "longitude": 3.5457254,
+    "type": "talk",
+    "color": "#D62828",
+    "permalink": "/talks/2025-12-17-Montpellier"
   },
   {
     "description": "Combining Finite Element methods and Neural Networks to solve elliptic problem on complex 2D geometries<br />École Normale Supérieure de Lyon, Lyon, France",
     "latitude": 45.7297811,
     "longitude": 4.8265819,
     "type": "poster",
-    "color": "#2E5CA6"
+    "color": "#2E5CA6",
+    "permalink": "/posters/2024-10-29-CJCMA"
+  },
+  {
+    "description": "Enriching continuous Lagrange finite element approximation spaces using neural networks<br />Politecnico di Milano, Milan, Italy",
+    "latitude": 45.4800101,
+    "longitude": 9.2282387,
+    "type": "poster",
+    "color": "#2E5CA6",
+    "permalink": "/posters/2025-03-24-workshop_milan"
+  },
+  {
+    "description": "Combining Finite Element methods and Neural Networks to solve elliptic problem on complex 2D geometries<br />UFR, Strasbourg, France",
+    "latitude": 48.5598541,
+    "longitude": 7.7445386,
+    "type": "poster",
+    "color": "#2E5CA6",
+    "permalink": "/posters/2024-07-08-workshop_stras"
   }
 ];
